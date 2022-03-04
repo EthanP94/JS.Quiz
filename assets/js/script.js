25d4bf0deec2ac6fced910d91542503ddf6d3000
EthanP94 <99214691+EthanP94@users.noreply.github.com>
2022-03-04 15:43:51 -0800

    added var funtion for startpage with containers. Wrote var funtion for timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,7 +26,7 @@ timerEl.innerText = 0;
 var highScores = [];
 var userScores = JSON.parse(localStorage.getItem("scores")) || []
 
-var userScore = {user:"dave",score:9}
+var userScore = {user:"",score:""}
 
 userScores.push(userScore)
 
@@ -37,28 +37,100 @@ var questions = [
     
   { q: 'What is the correct way to write a JavaScript array?', 
       a: '4. var colors = ["red", "green", "blue"] ', 
-      choices: ['1. var colors = (1:"red", 2:"green", 3:"blue")', '2. var colors = 1 = ("red"), 2 = ("green"), 3 = ("blue")', '3. var colors = "red", "green", "blue"  ', '4. var colors = ["red", "green", "blue"]']
+        c: ['1. var colors = (1:"red", 2:"green", 3:"blue")', '2. var colors = 1 = ("red"), 2 = ("green"), 3 = ("blue")', '3. var colors = "red", "green", "blue"  ', '4. var colors = ["red", "green", "blue"]']
     },
   { q: 'Inside which HTML element do we put the JavaScript?', 
-    a: '3. <script>', 
-    choices: [{choice: '1. <Script>'}, {choice: '2. <js>'}, {choice: '3. <script>'}, {choice: '4. <head>'}]
+      a: '3. <script>', 
+        c: ['1. <Script>','2. <js>','3. <script>', '4. <head>']
   },
   { q: 'What is the correct syntax for referring to an external script called "xxx.js"?',
-    a: '4. <script src="xxx.js"> ', 
-    choices: [{choice: '1. <script href="xxx.js">'}, {choice: '2. <script name="xxx.js">'}, {choice: '3. All of the Aobve'},{choice: '4. <script src="xxx.js'}]
+      a: '3. <script src="xxx.js"> ', 
+        c: ['1. <script href="xxx.js">','2. <script name="xxx.js">','3. <script src="xxx.js','4. All of the Above']
   }, 
-
+  { q: 'Where is the correct place to insert a JavaScript?',
+      a: '3. All of the above',
+        c: ['1. The <body> section','2. The <head> section', '3. All of the above']
+  },  
+  { q: 'How do you write "Hello World" in an alert box?',
+      a: '2. alert("Hello World")',
+        c: ['1. msgBox("Hello World")','2. alert("Hello World")','3. msg("Hello World")', '4. alertBox("Hello World")']
+  },
+  { q: 'How do you create a function in JavaScript?',
+      a: '1. function myFuntion()',
+        c: ['1. fuction myFunction()','2.function:myFunction()','3.function = myFunction()']
+  },
+  { q: 'How do you call a funtion named "myFunction"?',
+      a: '2. myFunction()',
+        c: ['1. call funtion myFunction()','2. myFunction()','3. call myFunction()']
+  },
+  { q: 'How to write an IF statement in JavaScript?',
+      a: '3. if (i == 5)',
+        c: ['1. if i = 5 then)','2.if i == 5 then','3. if (i == 5)','4. if i = 5']
+  },
+  { q:'How to write an IF statement for executing some code if "i" is NOT equal to 5?',
+      a: '4. if (i != 5)',
+      c: ['1. if i <> 5','2.if (i <> 5)','3. if i =! 5 then','4. if (i != 5)']
+  },
+  { q:'How does a WHILE loop start?',
+      a: '1. while (i <= 10)',
+      c: ['1. while (i <= 10)','2. while i = 1 to 10','3. while (i <= 10; i++)',]
+  },
+  { q:'How does a FOR loop start?',
+      a: '2. for (i = 0; i <= 5; i++)',
+      c: ['1. for i = 1 to 5','2.for (i = 0; i <= 5; i++)','3. for (i <= 5; i++)','4.for (i = 0; i <= 5)']
+  },
+  { q:'How can you add a comment in a JavaScript?',
+      a: '1. //This is a comment  ',
+      c: ['1. //This is a comment','2. This is a comment','3. <!--This is a comment-->']
+    },
   ];
 
+  var renderStartPage = function () {
+    containerHighScoresEl.classList.add("hide")
+    containerHighScoresEl.classList.remove("show")
+    containerStartEl.classList.remove("hide")
+    containerStartEl.classList.add("show")
+    containerScoreEl.removeChild(containerScoreEl.lastChild)
+    QuestionIndex = 0
+    gameover = ""
+    timerEl.textContent = 0 
+    score = 0
+
+    if (correctEl.className = "show") {
+        correctEl.classList.remove("show");
+        correctEl.classList.add("hide")
+    }
+    if (wrongEl.className = "show") {
+        wrongEl.classList.remove("show");
+        wrongEl.classList.add("hide");
+    }
+}
+
+var setTime = function () {
+  timeleft = 60;
+
+var timercheck = setInterval(function() {
+  timerEl.innerText = timeleft;
+  timeleft--
+
+  if (gameover) {
+      clearInterval(timercheck)
+  }
  
+  if (timeleft < 0) {
+      showScore()
+      timerEl.innerText = 0
+      clearInterval(timercheck)
+  }
+
+  }, 1000)
+}
   
-  
-  
-  // var position = 0
-    //document.getElementById("questions").innerText = questions[position].q
-  // button function
-  // nextbutton.onclick = position++
-  // //document.getElementById("questions").innerText = questions[position].q
+// var position = 0
+//document.getElementById("questions").innerText = questions[position].q
+// button functions
+// nextbutton.onclick = position++
+//document.getElementById("questions").innerText = questions[position].q
 
 // get the start button to show the first question 
 
@@ -68,4 +140,3 @@ var questions = [
 
 // funtion to stop game once timers/question run out 
 
-// 
